b43959e8dd7bf6890055f1558f9cd5acba75505f
AdrianoAnimus <adrianoanimus@gmail.com>
2021-08-17 00:24:16 -0300

    Creating firebase import

--- app-react/src/database/firebase.js
+++ app-react/src/database/firebase.js
@@ -1,5 +1,5 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
+import firebase from 'firebase'
+const  firebaseConfig = {
     apiKey: "AIzaSyBE1Gn_swqvxUlyGzaek929eDRS2HXl0C8",
     authDomain: "cadastropacientes-c2387.firebaseapp.com",
     projectId: "cadastropacientes-c2387",
@@ -7,5 +7,7 @@ var firebaseConfig = {
     messagingSenderId: "893035366822",
     appId: "1:893035366822:web:6ad302bd38e118d9d86767"
   };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+
+  let fireDb = firebase.initializeApp(firebaseConfig);
+
+  export default fireDb.database().ref()
