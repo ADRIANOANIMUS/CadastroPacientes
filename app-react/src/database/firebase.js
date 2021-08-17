b7b4e1e0660513e74c29b2174b199e1b549219ca
AdrianoAnimus <adrianoanimus@gmail.com>
2021-08-17 00:19:49 -0300

    Ccreating database connection

---
+++ app-react/src/database/firebase.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBE1Gn_swqvxUlyGzaek929eDRS2HXl0C8",
+    authDomain: "cadastropacientes-c2387.firebaseapp.com",
+    projectId: "cadastropacientes-c2387",
+    storageBucket: "cadastropacientes-c2387.appspot.com",
+    messagingSenderId: "893035366822",
+    appId: "1:893035366822:web:6ad302bd38e118d9d86767"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
