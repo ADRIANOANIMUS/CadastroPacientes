581077f99a0d18d78efca5eef946dde527439e00
AdrianoAnimus <adrianoanimus@gmail.com>
2021-08-18 20:55:22 -0300

    Insert "Jumbotron" component Bootstrap

--- app-react/src/components/Register.js
+++ app-react/src/components/Register.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
-const Register = () =>{
-    return(
-        <h1>Register</h1>
+const Register = () => {
+    return (
+        <div>
+            <h1>Register</h1>
+            <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                    <h1 class="display-4">Fluid jumbotron</h1>
+                    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                </div>
+            </div>
+
+
+        </div>
     )
 }
 export default Register
