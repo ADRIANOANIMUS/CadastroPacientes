f1b6e0015cbf3941409738c8d4b88d7cd46bda04
AdrianoAnimus <adrianoanimus@gmail.com>
2021-08-18 21:12:26 -0300

    Import "FormRegister"

--- app-react/src/components/Register.js
+++ app-react/src/components/Register.js
@@ -1,17 +1,21 @@
 import React from 'react'
+import FormRegister from './FormRegister'
 
 const Register = () => {
     return (
         <div>
-            <h1>Register</h1>
             <div class="jumbotron jumbotron-fluid">
                 <div class="container">
-                    <h1 class="display-4">Fluid jumbotron</h1>
+                    <h1 class="display-4">Cadastro de Pacientes</h1>
                     <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
                 </div>
             </div>
 
-
+            <div className="row">
+                <div className="col-md-5">
+                    <FormRegister/>
+                </div>
+            </div>            
         </div>
     )
 }
